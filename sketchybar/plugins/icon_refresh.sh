52f042f47187ac47ba727492bdd6bc944ca8234f
0xShishigami <shishigami@defi.sucks>
2026-03-14 18:02:12 -0300

    feat: add workspace-to-monitor assignment and improve sketchybar drawing logic

--- sketchybar/plugins/icon_refresh.sh
+++ sketchybar/plugins/icon_refresh.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-PLUGIN_DIR=/Users/timothy/.config/sketchybar/plugins
+PLUGIN_DIR="$CONFIG_DIR/plugins"
+
+for sid in $(aerospace list-workspaces --all); do
+    sketchybar --set space.$sid drawing=off
+done
+
 for mid in $(aerospace list-monitors | cut -c1); do
-    for sid in $(aerospace list-workspaces --monitor $mid); do
+    focused=$(aerospace list-workspaces --monitor "$mid" --visible)
+    for sid in $(aerospace list-workspaces --monitor "$mid"); do
         apps=$(aerospace list-windows --workspace "$sid" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
 
-        sketchybar --set space.$sid drawing=on
-
         icon_strip=" "
-        if [ "${apps}" != "" ]; then
+        if [ -n "$apps" ]; then
             while read -r app; do
                 icon_strip+=" $($PLUGIN_DIR/icon_map_fn.sh "$app")"
             done <<<"${apps}"
         else
             icon_strip=""
         fi
-        sketchybar --set space.$sid label="$icon_strip"
+
+        has_windows=$( [ -n "$apps" ] && echo "on" || echo "off" )
+        is_focused=$( [ "$sid" = "$focused" ] && echo "on" || echo "$has_windows" )
+
+        sketchybar --set space.$sid display=$mid drawing=$is_focused label="$icon_strip"
     done
 done
-
-echo "$apps"
-echo "$icon_strip"
